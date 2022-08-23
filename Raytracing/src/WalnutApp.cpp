53a6af090cb38e03a4bd62a7b40631570a463b5b
Melker Widén <melker.widen@gmail.com>
2022-08-23 14:01:13 +0200

    ep3 refactored renderer

--- Raytracing/src/WalnutApp.cpp
+++ Raytracing/src/WalnutApp.cpp
@@ -1,11 +1,13 @@
 #include <memory>
 
+#include "Renderer.h"
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
-#include "Walnut/Random.h"
 #include "Walnut/Timer.h"
+
+#include "Renderer.h"
 
 using namespace Walnut;
 
@@ -15,6 +17,7 @@ public:
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Settings");
+		ImGui::Text("Framerate: %.3f FPS", 1.0f / (m_LastRenderTime / 1000.0f));
 		ImGui::Text("Last render: %.3fms", m_LastRenderTime);
 		if (ImGui::Button("Render"))
 		{
@@ -28,10 +31,10 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		if (m_Image)
-		{
-		ImGui::Image(m_Image->GetDescriptorSet(), {(float)m_Image->GetWidth(), (float)m_Image->GetHeight()});
-		}
+		auto image = m_Renderer.GetFinalImage();
+		if (image)
+			ImGui::Image(image->GetDescriptorSet(), {(float)image->GetWidth(), (float)image->GetHeight()});
+		
 
 		ImGui::End();
 		ImGui::PopStyleVar();
@@ -43,31 +46,18 @@ public:
 	{
 
 		Timer timer;
-		
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
-		{
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
 
-		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
-		{
-			m_ImageData[i] = Random::UInt();
-			m_ImageData[i] |= 0xff000000;
-		}
-
-		m_Image->SetData(m_ImageData);
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render();
 
 		m_LastRenderTime = timer.ElapsedMillis();
 		
 	}
 	
 private:
-	std::shared_ptr<Image> m_Image;
+	Renderer m_Renderer;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
-	uint32_t* m_ImageData = nullptr;
-
+	
 	float m_LastRenderTime = 0.0f;
 };
 
